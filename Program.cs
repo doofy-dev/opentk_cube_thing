9896164bff1b3450eeeff523a6092e69c1a440d1
teeeebor@gmail.com <wateronetibi6206>
2015-09-20 12:51:42 +0200

    Comments, added Color change, removed form

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using cube_thing.renderEngine.core.entity;
 using cube_thing.renderEngine.render;
 using cube_thing.renderEngine.render.model;
 using cube_thing.renderEngine.render.shader.material;
+using OpenTK;
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -20,10 +21,34 @@ namespace cube_thing
         [STAThread]
         static void Main()
         {
+            /*
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormTest());
-           
+            Application.Run(new FormTest());*/
+            new DisplayManager(() =>
+            {
+                //CAMERA setup
+                GameObject camera = new GameObject("camera");   //Creating gameObject //String parameter is just a name
+                AbstractCamera cam = new AbstractCamera();  //Creating camera
+                cam.setMain();                              //Sets this camera as main
+                cam.setBackgroundColor(new Vector3(0,0,0)); //Sets background color  ->  RGB 0-1 at scale
+                camera.addComponent(cam);                   //Adding the camera component to the gameobject
+                World.getInstance().assingGameObject(camera);       //Adding gameobject to the world
+                camera.transform.position.Z = 10;                   //Setting camera gameobject's position
+
+                //Sample box setup
+                GameObject box = new GameObject("Box");         
+                DefaultMat material = new DefaultMat(); //Creating material component
+                material.setCullingEnabled(true);       //Backface culling enabled
+                Renderer m = new Renderer(Primitives.Cube(), material);     //Creating renderer component -> Parameters Primitives.PRIMITIVE_NAME, material
+                box.addComponent(m);                    //Assing renderer to the gameobject
+                World.getInstance().assingGameObject(box);  //Add gameobject to the world
+
+
+                material.setColor(new Vector4(1, 0, 0.1f, 1));      //Setting the materials color
+
+            });
+
         }
     }
 }
--- renderEngine/render/shader/material/DefaultMat.cs
+++ renderEngine/render/shader/material/DefaultMat.cs
@@ -1,4 +1,5 @@
 ﻿using cube_thing.renderEngine.render.shader.uniform;
+using OpenTK;
 using renderEngine.render.shader;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ namespace cube_thing.renderEngine.render.shader.material
 {
     public class DefaultMat : Material
     {
+        private UniformVec4 color;
         public DefaultMat() :base(new ShaderSource[]{
                 new ShaderSource("default.vert", GL_VERTEX_SHADER),
                 new ShaderSource("default.frag", GL_FRAGMENT_SHADER)
@@ -17,11 +19,16 @@ namespace cube_thing.renderEngine.render.shader.material
                 "textureCoords",
                 "normal"
         }, new UniformVariable[]{
+            new UniformVec4("color")
         })
         {
-
+            color = (UniformVec4)locations["color"];
         }
 
- 
+        public void setColor(Vector4 c)
+        {
+            color.set(c);
+        }
+
     }
 }
